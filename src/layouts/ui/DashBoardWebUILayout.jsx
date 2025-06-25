fae4b04ad2e346d7af4c337d8c8b63bc2eb677b6
KenTheWhale <kienquoc11026789@gmail.com>
2025-06-25 21:58:19 +0700

    fix: fix conflict

---
+++ src/layouts/ui/DashBoardWebUILayout.jsx
@@ -0,0 +1,68 @@
+import '../../styles/ui/DashBoardWebUILayout.css'
+import {Outlet} from 'react-router-dom'
+import {Link, Typography} from "@mui/material";
+
+function RenderNav({navigation}) {
+
+    function RenderNavCaption({label}) {
+        return (
+            <Typography variant={"h6"} sx={{marginTop: '2vh'}}>{label}</Typography>
+        )
+    }
+
+    function RenderNavSub({label, segment}) {
+        return (
+            <Link
+                href={segment}
+                variant={"body2"}
+                underline={"none"}
+                color={"textPrimary"}
+                sx={
+                    {
+                        marginLeft: "2vw",
+                        cursor: 'pointer'
+                    }
+                }
+            >
+                {label}
+            </Link>
+        )
+    }
+
+    return (
+        <div>
+            {
+                navigation.map((el, index) => {
+                    if (el.type === 'caption') {
+                        return (
+                            <div key={index}><RenderNavCaption label={el.label}/></div>
+                        )
+                    } else {
+                        return (
+                            <div key={index}><RenderNavSub label={el.label} segment={el.segment}/></div>
+                        )
+                    }
+                })
+            }
+        </div>
+    )
+}
+
+function RenderPage({navigation}) {
+    return (
+        <div className={'dbwui-main'}>
+            <div className={'dbwui-nav'}>
+                <RenderNav navigation={navigation}/>
+            </div>
+            <div className={'dbwui-outlet'}>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
+
+export default function DashBoardWebUILayout({navigation}) {
+    return (
+        <RenderPage navigation={navigation}/>
+    )
+}
